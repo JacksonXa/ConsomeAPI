5d646e6c8c87048687665f2a457d1be655b63c32
unknown <ja.xavier06@gmail.com>
2023-07-26 10:57:16 -0300

    API de consunta de CEP

---
+++ ConsumindoAPIdeCEP/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Newtonsoft.Json;
+
+namespace ConsumindoAPIdeCEP
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient { BaseAddress = new Uri("https://viacep.com.br/ws/") };
+
+            Console.WriteLine("Informe seu CEP (apenas numeros): ");
+            var cep = Console.ReadLine();
+
+            var response = await client.GetAsync(cep+"/json");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var dadosEndereco = JsonConvert.DeserializeObject<Endereco>(content);
+
+            Console.WriteLine(dadosEndereco.Logradouro);
+        }
+    }
+}
